128cc0b3801a02aae8db550764ce42d2202f437b
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-08-30 15:02:15 -0300

    messed with tabs and stacks navigator

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { SignIn, CreateAccount, Profile, Home, Search, Details, Search2 } from './Components/Screens';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Tabs = createBottomTabNavigator();
+const HomeStack = createNativeStackNavigator();
+const SearchStack = createNativeStackNavigator();
+
+const HomeStackScreen = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen name="Home" component={Home} />
+    <HomeStack.Screen name="Details" component={Details} options={({ route }) => ({ title: route.params.name })} />
+  </HomeStack.Navigator>
+);
+
+const SearchStackScreen = () => (
+  <SearchStack.Navigator>
+    <SearchStack.Screen name="Search" component={Search} />
+    <SearchStack.Screen name="Search2" component={Search2} />
+  </SearchStack.Navigator>
+);
+
+export default () => (
+  <NavigationContainer>
+    <Tabs.Navigator>
+      <Tabs.Screen name="Home" component={HomeStackScreen} options={{ headerShown: false }} />
+      <Tabs.Screen name="Search" component={SearchStackScreen} options={{ headerShown: false }} />
+    </Tabs.Navigator>
+    {/* <AuthStack.Navigator>
+      <AuthStack.Screen 
+        name="SignIn" 
+        component={SignIn} 
+        options={{ title: "Sign In" }} 
+      />
+      <AuthStack.Screen 
+        name="CreateAccount" 
+        component={CreateAccount} 
+        options={{ title: "Create Account" }} 
+      />
+    </AuthStack.Navigator> */}
+  </NavigationContainer>
+)
